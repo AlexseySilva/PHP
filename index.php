15e369752435842c0795c3685a440bb6030f04c7
Alexsey <83961167+AlexseySilva@users.noreply.github.com>
2023-05-19 04:58:56 -0300

    Time

--- index.php
+++ index.php
@@ -4,16 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Constantes</title>
+    <title>PHP</title>
 </head>
 <body>
-    <h1>Entendendo constantes</h1>
-    <?php 
-    const COM_UNI_CODE = "Alexsey \u{1f418} para usar ini code use aspas duplas <br>";
-    const SEM_UNI_CODE = 'Alexsey \u{1f418} sem unicode aspas unicas ';
-    echo COM_UNI_CODE . SEM_UNI_CODE;
-
-    
+    <h1>
+        Exemplo de PHP
+    </h1>
+    <?php
+        //phpinfo();
+        //date_default_timezone_set("America/Sao_Paulo")
+        echo "hoje é dia" . date("d/M/Y");
+        echo " e a hora atual é" . date("G:i:s:T");
     ?>
+    
 </body>
 </html>
