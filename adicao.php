0354c412c0546affef338c10d6f53b5f4f608e89
Alexsey <83961167+AlexseySilva@users.noreply.github.com>
2023-05-19 05:48:19 -0300

    Add files via upload
    Adição

---
+++ adicao.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios de matematica</title>
+</head>
+<body>
+    <h1>
+        Exercicios de matematica
+    </h1>
+    <?php 
+    echo "Olá, mundo! \u{1F30E}";
+
+    $num = 12;
+    $ant = $num - 1;
+    $suc = $num + 1;
+    echo "O antecessor de $num é $ant <br>";
+    echo "O antecessor de $num é $suc <br>";
+    echo "A segunda forma de fazer a conta seria <br>";
+    echo "O antecessor de $num é". ($num -1) ."<br>";
+    echo "O sucessor de $num é". ($num +1) ."";
+    ?>
+</h1>
+</body>
+</html>
